22fdeea55c3b1fd26a27c7b7a9fd7725f00f3333
Raghvendra Singh <raghvendrrsingh@gmail.com>
2025-08-28 15:43:41 +0530

    Add /me API

--- api/__init__.py
+++ api/__init__.py
@@ -10,6 +10,7 @@ from api.workout_logs import router as workout_log
 from api.generate_plans import router as generate_ai_plan
 from api.dashboard import router as dashboard_analytics
 from api.plans import router as plans
+from api.users import router as users
 
 # Register routes with their own sub-prefixes
 api_router.include_router(auth_router, tags=["Authentication"])
@@ -18,3 +19,4 @@ api_router.include_router(workout_log, tags=["Workout Logs"])
 api_router.include_router(generate_ai_plan, tags=["Generate AI Plan"])
 api_router.include_router(dashboard_analytics, tags=["Dashboard"])
 api_router.include_router(plans, tags=["Plans"])
+api_router.include_router(users, tags=["Users"])
